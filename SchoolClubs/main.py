565d9b7a09f683a868448505e4062c56973616af
God-Of-Code3 <64793044+God-Of-Code3@users.noreply.github.com>
2021-03-31 23:21:03 +0300

    Добавление функционала странице teachers

--- SchoolClubs/main.py
+++ SchoolClubs/main.py
@@ -4,7 +4,7 @@ import random
 from datetime import datetime
 from statement import createStatement
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="static", )
 
 
 def main():
@@ -15,6 +15,7 @@ def main():
 @app.route('/index/')
 def index():
     params = dict()
+    params['menu_current_page'] = 'main'
     params["clubs"] = []
     clubs = manager.get_items("club", "1")
     for club in clubs:
@@ -38,93 +39,97 @@ def index():
         }
     ]
 
-    params["teachers_rows"] = [
-        [
-            {
-                "id": 1,
-                "description": "Сталкер",
-                "image": "teachers/melnik.jpg",
-                "name": "Святослав",
-                "surname": "Мельников",
-                "middle_name": "Константинович",
-            },
-            {
-                "id": 1,
-                "description": "Сталкер",
-                "image": "teachers/melnik.jpg",
-                "name": "Святослав",
-                "surname": "Мельников",
-                "middle_name": "Константинович",
-            },
-            {
-                "id": 1,
-                "description": "Сталкер",
-                "image": "teachers/melnik.jpg",
-                "name": "Святослав",
-                "surname": "Мельников",
-                "middle_name": "Константинович",
-            },
-            {
-                "id": 1,
-                "description": "Сталкер",
-                "image": "teachers/melnik.jpg",
-                "name": "Святослав",
-                "surname": "Мельников",
-                "middle_name": "Константинович",
-            }
-        ]
-    ]
+    params["teachers_rows"] = []
+    teachers = manager.get_items("user", "`type_id` = '3' LIMIT 8")
+    for i, teacher in enumerate(teachers):
+        if i % 4 == 0:
+            params["teachers_rows"].append([])
+        params["teachers_rows"][-1].append({
+            "href": '..' + url_for('teacher', teacher_id=str(teacher.get('id'))),
+            "description": teacher.get('description'),
+            "image": teacher.get('image'),
+            "name": teacher.get('name'),
+            "surname": teacher.get('surname'),
+            "middle_name": teacher.get('middle_name')
+        })
     return render_template('index.html', params=params)
 
 
 @app.route('/teachers/')
 def teachers():
     params = dict()
-    params["clubs"] = []
-    clubs = manager.get_items("club", "1")
-    for club in clubs:
-        params["clubs"].append({"id": club.get('id'), "name": club.get('name')})
-    params["teachers_row"] = [
-        [
-            {
-                "id": 1,
-                "description": "Учитель пинания бревна",
-                "image": "teachers/1.jpg",
-                "name": "Пинок",
-                "surname": "Пинков",
-                "middle_name": "Пинкович",
-            }
-        ]
-    ]
-    return render_template('index.html', params=params)
+    params['menu_current_page'] = 'teachers'
+    params["teachers_rows"] = []
+    teachers = manager.get_items("user", "`type_id` = '3'")
+    for i, teacher in enumerate(teachers):
+        if i % 4 == 0:
+            params["teachers_rows"].append([])
+        params["teachers_rows"][-1].append({
+            "href": '..' + url_for('teacher', teacher_id=str(teacher.get('id'))),
+            "description": teacher.get('description'),
+            "image": teacher.get('image'),
+            "name": teacher.get('name'),
+            "surname": teacher.get('surname'),
+            "middle_name": teacher.get('middle_name')
+        })
+    params["all_teachers"] = True
+    return render_template('all_teachers.html', params=params)
 
 
 @app.route('/teacher/<teacher_id>/')
 def teacher(teacher_id):
+    tc = User(manager, id=teacher_id).fields
+    gr = manager.get_items('club_group', 'teacher_id=' + teacher_id)
+    cl = []
+    if gr:
+        for group in gr:
+            c = Club(manager, id=group.fields["club_id"]).fields
+            if c not in cl:
+                cl.append(c)
+
+    information = [{"id": club["id"], "name": "<h2>" + club["name"] + "</h2>"} for club in cl]
+    name = tc['surname'] + ' ' + tc['name'] + ' ' + tc['middle_name']
+
+    if gr:
+        num_of_clubs = len(cl)
+        if 5 <= num_of_clubs <= 20:
+            num_of_clubs = " ведет <b>" + str(num_of_clubs) + "</b> кружков:"
+        elif 2 <= num_of_clubs % 10 <= 4:
+            num_of_clubs = " ведет <b>" + str(num_of_clubs) + "</b> кружка:"
+        elif num_of_clubs % 10 == 1:
+            num_of_clubs = " ведет <b>" + str(num_of_clubs) + "</b> кружок:"
+        else:
+            num_of_clubs = " ведет <b>" + str(num_of_clubs) + "</b> кружков:"
+    else:
+        num_of_clubs = " не ведет кружков."
+
     params = dict()
-    params["clubs"] = []
-    params["clubs_search_form"] = False
-    params["clubs_title"] = "Кирсанов Максим Григорьевич ведет <b>2</b> кружка:"
-    params["clubs_list"] = [
-        {
-            "id": 1,
-            "name": "<h2>Математика</h2>"
-        },
-        {
-            "id": 2,
-            "name": "<h2>Робототехника</h2>"
-        }
-    ]
-    params["teacher_name"] = "Кирсанов Максим Григорьевич"
-    params["teacher_description"] = "Учитель информатики и математики"
-    params["teacher_image"] = "3.jpg"
+    params['menu_current_page'] = 'teachers'
+    params["clubs"] = cl
+    if len(cl) > 10:
+        params["clubs_search_form"] = True
+    else:
+        params["clubs_search_form"] = False
+    params["clubs_title"] = name + num_of_clubs
+    params["clubs_list"] = information
+    params["teacher_name"] = name
+    params["teacher_description"] = tc["description"]
+    params["teacher_image"] = tc["image"]
     params["clubs_show_button"] = False
     return render_template('teacher.html', params=params)
+
+
+@app.route('/clubs/')
+def clubs():
+    params = dict()
+    params['menu_current_page'] = 'clubs'
+    return render_template('all_clubs.html', params=params)
 
 
 @app.route('/club/<club_id>/')
 def club(club_id):
     params = dict()
+    params['menu_current_page'] = 'clubs'
     params["screen_image"] = "clubs/1.jpg"
     params["screen_title"] = "Робототехника"
 
@@ -219,7 +224,7 @@ def statement_handler():
         else:
             if len(users_statements) < 10:
                 new_statement.add_to_db()
-        createStatement(manager, str(Statement(manager, 'track_code=' + args['track_code']).fields['id']))
+        createStatement(manager, str(new_statement.fields['id']))
         return redirect("http://yandex.ru/maps", code=302)
     return ''
 
